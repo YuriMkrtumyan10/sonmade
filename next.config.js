02b3caac654bf55aa8d46615393e6876f097c165
YuriMkrtumyan10 <yurimkrtumyan02@gmail.com>
2025-07-23 00:18:20 +0400

    modified next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,27 @@
 const path = require('path');
 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   distDir: process.env.NEXT_DIST_DIR || '.next',
+//   output: process.env.NEXT_OUTPUT_MODE,
+//   // experimental: {
+//   //   outputFileTracingRoot: path.join(__dirname, '../'),
+//   // },
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: false,
+//   },
+//   images: { unoptimized: true },
+// };
+
+// module.exports = nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
+  // distDir and output are fine as is, or you can remove them if not needed
   eslint: {
     ignoreDuringBuilds: true,
   },
